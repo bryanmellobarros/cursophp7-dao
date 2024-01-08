d055e04e666b8d39d2990f6bc4181d0c38c0b7b4
Bryan <bryanmellobarros@gmail.com>
2024-01-08 11:34:11 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,31 @@
 
 require_once("config.php");
 
+/*Traz apenas um usuário
 $root = new Usuario();
 
 $root->loadById(3);
 
 echo $root;
+*/
+
+/*Retorna lista com todos os usuários
+$lista=Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+/*Carrega uma lista de usuários buscando pelo login
+$search=Usuario::search("ro");
+
+echo json_encode($search);
+
+*/
+
+$usuario = new Usuario();
+
+$usuario->login("root", "!@#$%");
+
+echo $usuario;
 
 ?>
